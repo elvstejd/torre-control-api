55f0d37d085d5e93003c1d64591acac6b5fecfcb
Elvis Tejeda <elvistejeda0910@gmail.com>
2023-05-25 21:39:11 -0400

    ejecutar la app desde main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from routers import aviones, pasajeros, aeropuertos
 from db import engine
 from fastapi import FastAPI
 import models
+import uvicorn
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
@@ -14,3 +15,8 @@ app.include_router(aeropuertos.router)
 @app.get("/")
 def root():
     return {"message": "Bienvenido a la API de Torre de Control"}
+
+
+# run app with uvicorn in reload mode
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=3002, reload=True)

a3846f27d6c19e72f8004d5bccc5297e187647f8
Elvis Tejeda <elvistejeda0910@gmail.com>
2023-05-26 16:27:17 -0400

    documentar instalacion y uso

--- app/main.py
+++ app/main.py
@@ -22,4 +22,4 @@ def root():
 
 # run app with uvicorn in reload mode
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=3002, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
